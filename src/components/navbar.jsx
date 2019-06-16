5f666bf973883a1fd75640ac4c4a96935cf6c67e
Ikhwan Noor Ikhsan <23516049@std.stei.itb.ac.id>
2019-06-16 14:58:48 +0800

    version 1.0.0

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 //Stateless Functional Component
-const Navbar = props => {
+const Navbar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a href="" className="navbar-brand">
         Navbar
         <span className="badge badge-pill badge-secondary">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </a>
     </nav>
